d318670e5158b50aa803b613cc4ac120bc74d992
25-Otabek <valiboyevo@gmail.com>
2023-09-01 11:36:05 +0500

    Add feature section

---
+++ js/main.js
@@ -0,0 +1,34 @@
+const elsTabsItem = document.querySelectorAll('.feature__item');
+const elsTabLink = document.querySelectorAll('.js-feature-link');
+const elsTabBox = document.querySelectorAll('.feature__box');
+
+function deactivateTabItems() {
+  elsTabsItem.forEach(function (elTabItem) {
+    elTabItem.classList.remove('feature__item--active')
+  });
+}
+
+function deactivateTabBox() {
+  elsTabBox.forEach(function (elTabBox) {
+    elTabBox.classList.remove('feature__box-active')
+  });
+}
+
+elsTabLink.forEach(function (elTabLink) {
+  elTabLink.addEventListener('click', function (evt) {
+    //Links have been restored
+    evt.preventDefault();
+
+    //Activity has been removed from tab items
+    deactivateTabItems();
+
+    //Activity has been removed from tab box
+    deactivateTabBox();
+
+    const activePanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
+    activePanel.classList.add('feature__box-active')
+
+    //Added active class when tab items are active
+    elTabLink.parentElement.classList.add('feature__item--active');
+  })
+})
